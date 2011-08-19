cbd42e3edfd0037bae00c5bd24ac45067e7552e0
ariefhidayatulloh@gmail.com <ariefhidayatulloh@gmail.com@e2ea1595-e6d1-35ab-287f-b402355e3e36>
2011-08-19 07:29:41 +0000

    Awal

---
+++ batan/silaba/view/sidebar_psjmn.php
@@ -0,0 +1,100 @@
+
+
+<!--                        <h3>recent blog entries</h3>-->
+                        <ul class="blog-entries">
+                            <li class="bd">
+                                <ul>
+                                    <li class="image">
+                                        <a href="?l=psjmn&s=jadwal">
+                                            <img src="images/psjmn/Calendar 2.png" alt="alternative text for image" width="64px" height="64" title="image title here" >
+                                        </a>
+
+                                    </li>                                    
+                                    <li class="author">&nbsp;</li>
+                                    <li class="title"><span><a href="?l=psjmn&s=jadwal" title="link title">Jadwal Layanan</a></span></li>
+                                    <li class="date">&nbsp;</li>
+                                </ul>
+                            </li>
+
+                            <li class="bd">
+
+                                <ul>
+                                    <li class="image">
+                                        <a href="?l=psjmn&s=alur">
+                                            <img src="images/psjmn/alur.png" width="64px" height="64px" alt="alternative text for image" title="image title here" >
+                                        </a>
+
+                                    </li>
+                                    <li class="author">&nbsp;</li>
+                                    <li class="title"><span><a href="?l=psjmn&s=alur" title="link title">Alur Proses Sertifikasi</a></span></li>
+                                    <li class="date">&nbsp;</li>
+                                </ul>
+                            </li>
+
+                            <li class="bd">
+                                <ul>
+                                    <li class="image">
+                                        <a href="?l=psjmn&s=syarat">
+                                            <img src="images/psjmn/Clipboard.png" alt="alternative text for image" width="64px" height="64px" title="image title here" >
+
+                                        </a>
+
+                                    </li>                                    
+                                    <li class="author">&nbsp;</li>
+                                    <li class="title"><span><a href="?l=psjmn&s=syarat" title="link title">Persyaratan</a></span></li>
+                                    <li class="date">&nbsp;</li>
+                                </ul>
+                            </li>
+
+                            <li class="bd">
+                                <ul>
+                                    <li class="image">
+                                        <a href="?l=psjmn&s=daftar">
+                                            <img src="images/psjmn/User 2.png" width="64px" height="64px"  alt="alternative text for image" title="image title here" >
+
+                                        </a>
+
+                                    </li>                                    
+                                    <li class="author">&nbsp;</li>
+                                    <li class="title"><span><a href="?l=psjmn&s=daftar" title="link title">Pendaftaran Online</a></span></li>
+                                    <li class="date">&nbsp;</li>
+                                </ul>
+                            </li>
+
+                        </ul>
+<!--                        <h3>client testimonials</h3>
+                        <ul>
+                            <li class="testimonial">
+                                <ul>
+                                    <li class="extract">"worked wonders for our business..."</li>
+                                    <li class="testimonial-main">The works they did for us was absolutely fantastic.  They really worked wonders for our business, which before them was floundering.</li>
+                                    <li class="testimonial-author">by Michael Clarke - Made up company</li>
+
+                                </ul>
+                            </li>
+                            <li class="testimonial">
+                                <ul>
+                                    <li class="extract">"worked wonders for our business..."</li>
+                                    <li class="testimonial-main">The works they did for us was absolutely fantastic.  They really worked wonders for our business, which before them was floundering.</li>
+                                    <li class="testimonial-author">by Michael Clarke - Made up company</li>
+
+                                </ul>
+                            </li>
+                            <li class="testimonial">
+                                <ul>
+                                    <li class="extract">"worked wonders for our business..."</li>
+                                    <li class="testimonial-main">The works they did for us was absolutely fantastic.  They really worked wonders for our business, which before them was floundering.</li>
+                                    <li class="testimonial-author">by Michael Clarke - Made up company</li>
+
+                                </ul>
+                            </li>
+                        </ul>-->
+                        <div class="outerhol">
+                            <div class="search">
+                                <form action="" method="get">
+                                    <input name="search" type="text" class="textfield">
+                                    <input name="search" type="button" value="search" class="button">
+                                </form>
+
+                            </div>
+                        </div>
