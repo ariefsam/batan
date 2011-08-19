550c8b1356dc25a513d829cfcdba3f80ba787249
ariefhidayatulloh@gmail.com <ariefhidayatulloh@gmail.com@e2ea1595-e6d1-35ab-287f-b402355e3e36>
2011-08-19 06:50:50 +0000

    Awal

---
+++ batan/kompugel_config/config.php
@@ -0,0 +1,17 @@
+<?php
+class config {
+// Contoh: http://example.com/
+
+var $base_url = "http://localhost/batan/batan/";
+
+
+// Database
+var $host       = "localhost";
+var $username   = "root";
+var $password   = "";
+var $dbname    = "batan";
+
+
+}
+
+$config = new config();
